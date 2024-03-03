32ebd48007cdaf06071902fcff2c847efd9ae1a3
Oprin Marius <marius@oprin.ro>
2024-03-03 15:18:21 +0200

    Add install script and README

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# argocd-gitops
+# GitOps with ArgoCD and ClusterAPI
+
+## Prerequisites
+
+    - kubectl installed
+    - secrets configured
+
+## Configure secrets
+
+The following secrets are required in the [secrets/kind](secrets/kind) folder:
+
+* CAPI AWS credentials:
+
+```
+---
+apiVersion: v1
+kind: Secret
+metadata:
+ name: aws-variables
+ namespace: default
+type: Opaque
+data:
+ AWS_B64ENCODED_CREDENTIALS: WW91J3ZlIGJlZW4gUmljayBSb2xsZWQK==
+```
+
+* GitHub repository credentials
+
+```
+apiVersion: v1
+kind: Secret
+metadata:
+  name: github
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+stringData:
+  type: git
+  url: git@github.com:CloudNative-Solutions/gitops.git
+  sshPrivateKey: |
+    -----BEGIN OPENSSH PRIVATE KEY-----
+    WW91J3ZlIGJlZW4gUmljayBSb2xsZWQK==
+    -----END OPENSSH PRIVATE KEY-----
+```
+
+## Install
+
+```
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+KEY_PAIR=cns-capi
+export ARGOCD_NS=argocd
+ARGO_CHART=argo-cd
+
+create_key_pair() {
+    local key_name=$1
+    aws ssm put-parameter --name "/sigs.k8s.io/cluster-api-provider-aws/ssh-key" \
+    --type SecureString \
+    --value "$(aws ec2 create-key-pair --key-name ${key_name} --output json | jq .KeyMaterial -r)"
+}
+
+create_kind_cluster() {
+    kind create cluster
+    kubectl cluster-info --context kind-kind
+}
+
+apply_secrets() {
+    kubectl apply -f ./secrets/kind
+}
+
+install_argocd() {
+    # Install ArgoCD with sync-waves enabled
+    helm repo add argo-cd https://argoproj.github.io/argo-helm
+    helm repo update
+    helm upgrade --install ${ARGO_CHART} --create-namespace --namespace ${ARGOCD_NS} -f values/kind/argo-cd/values.yaml argo-cd/argo-cd
+    echo "Waiting for ArgoCD deployment to be ready"
+    until kubectl wait deployment -n ${ARGOCD_NS} ${ARGO_CHART}-argocd-server --for condition=Available=True --timeout=90s; do sleep 1; done
+    until kubectl wait deployment -n ${ARGOCD_NS} ${ARGO_CHART}-argocd-applicationset-controller --for condition=Available=True --timeout=90s; do sleep 1; done
+    until kubectl wait deployment -n ${ARGOCD_NS} ${ARGO_CHART}-argocd-repo-server --for condition=Available=True --timeout=90s; do sleep 1; done
+}
+
+create_argo_apps() {
+    kubectl apply -f ./kind/argo-apps.yaml
+    until kubectl wait application -n ${ARGOCD_NS} argo-apps --for jsonpath='{.status.sync.status}'=Synced; do sleep 1; done
+}
+
+argo_ui() {
+    ARGO_PASS=$(kubectl get secret argocd-initial-admin-secret -n ${ARGOCD_NS} -o jsonpath='{.data.password}' | base64 -d )
+    echo ArgoCD credentials:
+    echo   User: admin
+    echo   Password: ${ARGO_PASS}
+    echo   Url: https://localhost:8080
+    echo Starting port forward:
+    kubectl port-forward svc/${ARGO_CHART}-argocd-server -n ${ARGOCD_NS} 8080:443
+}
+
+create_key_pair ${KEY_PAIR}
+create_kind_cluster
+install_argocd
+apply_secrets
+create_argo_apps
+argo_ui
+
